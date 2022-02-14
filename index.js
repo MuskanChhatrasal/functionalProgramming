8d7a3a02eaef85f2f533574381c6e7c1fb06fb00
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-02-14 02:46:30 +0000

    Done Question5

--- index.js
+++ index.js
@@ -38,4 +38,11 @@
   // const lessThan10OrNot = num => num < 10;
   // console.log(arr.filter(lessThan10OrNot));
 
+// Q5. Given an array of numbers written an object for each item.
+
+// Ans5: 
+  // const arr = [1, 5, 8, 10, 14, 25, 12];
+  // const arrayToObject = num => ({num});
+  // console.log(arr.map(arrayToObject));
+
 
