6f09706fa324ee7073b70c3361bf4dc15305f919
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-02-14 02:11:04 +0000

    Done Question2

--- index.js
+++ index.js
@@ -4,8 +4,16 @@
 //             name: 'aaaa',
 //             age: 20
 //        };
-
 // const siblingObj = {...myObj, age: myObj.age-2};
 // console.log(siblingObj);
 // console.log(myObj === siblingObj);
+
+// ------------------------------------------------
+
+// Q2. Take an array with 5 colors. Create another array by adding two more colors.
+
+// Ans2: 
+  // const color = ['red', 'blue', 'green', 'yellow','orange'];
+  // const addTwoMoreColor = [...color, 'pink', 'purple'];
+  // console.log(addTwoMoreColor);
 
