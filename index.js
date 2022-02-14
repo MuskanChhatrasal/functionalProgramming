05141eb81deeb4da78199cbed2d2b2f9e50eda79
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-02-14 02:04:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Q1. Take an object with your mother's name and your age. Now create an object for your sibling by age difference.
+
+const myObj = {
+  name: 'aaaa',
+  age: 20
+};
+
+const siblingObj = {...myObj, age: myObj.age - 2};
+console.log(siblingObj);
+console.log(myObj === siblingObj);
